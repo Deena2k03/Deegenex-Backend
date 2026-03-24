709a7ef879a9a18fd0fd9d02f9aa6ba0339946a3
Deena <mr.deena2k03@gmail.com>
2026-03-25 00:27:54 +0530

    Update jobs serializers.py

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -3,7 +3,16 @@ from .models import Job
 
 
 class JobSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
 
     class Meta:
         model = Job
         fields = "__all__"
+
+    def get_image(self, obj):
+        request = self.context.get('request')
+
+        if obj.image:
+            return request.build_absolute_uri(obj.image.url)
+
+        return None
